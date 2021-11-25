5b03c7429a0311d8b46ace8d0c9de2fb83aac990
GyoHeon Lee <ghl12078@gmail.com>
2021-11-25 21:23:56 +0900

    feat: members array make
    member object(id, accScore, nowScore) add to array

---
+++ sub.js
@@ -0,0 +1,6 @@
+//id 미정
+//const numOfPeople = document.querySelector(".input");
+n = 5;
+const arr = Array.apply(null, Array(n)).map(function () {});
+
+arr.forEach((x, i) => (arr[i] = { id: 1, accScore: 0, nowScore: 0 }));
