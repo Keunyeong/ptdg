a0041f1ac95d8c85a05a4d6e191a0dbd1498b629
GyoHeon Lee <ghl12078@gmail.com>
2021-11-25 21:38:19 +0900

    feat: add table template literal
    table only has one column

--- sub.js
+++ sub.js
@@ -4,3 +4,11 @@ n = 5;
 const arr = Array.apply(null, Array(n)).map(function () {});
 
 arr.forEach((x, i) => (arr[i] = { id: 1, accScore: 0, nowScore: 0 }));
+
+let table = `<table>
+              <tr>
+                <th>차례</th>
+                <th>1회</th>
+                <th>점수</th>
+              </tr>
+            </table>`;
