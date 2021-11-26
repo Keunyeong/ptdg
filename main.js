3cebf64a784b8cad702c9080ab66b6f97f1f8802
dongduu <4012popo@naver.com>
2021-11-26 19:19:43 +0900

    feat: Create Score Logic

---
+++ main.js
@@ -0,0 +1,18 @@
+let currentScore = 0;
+let diceNum = // 랜덤 주사위로 나온 숫자
+
+if (diceNum === 1) {
+    currentScore = 0;
+    // 다음 사람 턴
+}   else {
+    currentScore += diceNum;
+        if (currentScore >= 100) {
+            // 게임 종료
+        } else {
+            if(// 실행 o) {
+                // 주사위 함수 실행
+            } else {
+                // 다음 사람 턴
+            }
+        }
+}
